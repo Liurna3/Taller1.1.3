819f174338638b36d32ef42434f0d1c47f7b53f2
egrojtorrez <egrojtorrez@gmail.com>
2022-09-05 19:59:30 -0700

    Añadiendo Java Spring

---
+++ Spring/demo/src/main/java/com/example/demo/DemoApplication.java
@@ -0,0 +1,23 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class DemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoApplication.class, args);
+    }
+
+    @GetMapping("/hola")
+    public String HolaMundo(@RequestParam(value = "nombre", defaultValue = "Mundo") String name)
+    {
+        return String.format("Hola %s!", name);
+    }
+
+}
